bf70a647b585483092fdd410c889a350262bfd2f
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-04-18 12:56:46 +0200

    getTodos clientes

--- src/main/java/modelo/ClienteModelo.java
+++ src/main/java/modelo/ClienteModelo.java
@@ -1,5 +1,37 @@
 package modelo;
 
-public class ClienteModelo {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+public class ClienteModelo extends Conector{
+	
+	public ArrayList<Cliente> getTodos() {
+        ArrayList<Cliente> clientes = new ArrayList<>();
+        
+        Conector conector = new Conector();
+        
+        try {
+            Statement st = conexion.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM CLIENTES");
+            while (rs.next()) {
+            	Cliente cliente = new Cliente();
+                
+            	cliente.setId(rs.getInt("ID_CLIENTE"));
+            	cliente.setNombre(rs.getString("NOMBRE_CLIENTE"));
+            	
+            	cliente.setTelefono(rs.getString("TELEFONO"));
+            	
+            	cliente.setEmail(rs.getString("EMAIL"));
+                
+                clientes.add(cliente);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return clientes;
+    }
 }
