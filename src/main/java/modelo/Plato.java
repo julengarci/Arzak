f3ea7e832e60a9a8b5e63200079fbcddb3b2806e
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-04-18 12:32:43 +0200

    clase plato

--- src/main/java/modelo/Plato.java
+++ src/main/java/modelo/Plato.java
@@ -1,5 +1,43 @@
 package modelo;
 
-public class Plato {
+import java.util.ArrayList;
 
+public class Plato {
+	
+	private int id;
+    private String nombre;
+    private Tipo tipo;
+    private ArrayList<Ingrediente> ingredientes;
+    
+    
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Tipo getTipo() {
+		return tipo;
+	}
+	public void setTipo(Tipo tipo) {
+		this.tipo = tipo;
+	}
+	public ArrayList<Ingrediente> getIngredientes() {
+		return ingredientes;
+	}
+	public void setIngredientes(ArrayList<Ingrediente> ingredientes) {
+		this.ingredientes = ingredientes;
+	}
+	
+	@Override
+	public String toString() {
+		return "Plato [id=" + id + ", nombre=" + nombre + ", tipo=" + tipo + ", ingredientes=" + ingredientes + "]";
+	}
+	
 }
