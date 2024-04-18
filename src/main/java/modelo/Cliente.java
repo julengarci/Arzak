90180eb53f1b9b08fa84bd69a4f91d9b5de20377
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-04-18 12:16:05 +0200

    clase cliente

--- src/main/java/modelo/Cliente.java
+++ src/main/java/modelo/Cliente.java
@@ -1,5 +1,41 @@
 package modelo;
 
 public class Cliente {
-
+	
+	private int id;
+	private String nombre;
+	private String telefono;
+	private String email;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getTelefonoString() {
+		return telefono;
+	}
+	public void setTelefonoString(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nombre=" + nombre + ", telefonoString=" + telefono + ", email=" + email
+				+ "]";
+	}
 }
