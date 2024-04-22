837238bb02a47ae4d517708a63012792f020c806
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-04-22 19:24:07 +0200

    Ingrediente terminado ( eliminar, editar, ver todos y ver uno funcionales ) a falta de añadir el crear y habria que cambiar el alergeno por una checkbox

--- src/main/java/controlador/DeleteIngrediente.java
+++ src/main/java/controlador/DeleteIngrediente.java
@@ -7,20 +7,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import modelo.Cliente;
-import modelo.ClienteModelo;
+import modelo.IngredienteModelo;
 
 /**
- * Servlet implementation class UpdateCliente
+ * Servlet implementation class DeleteIngrediente
  */
-@WebServlet("/UpdateCliente")
-public class UpdateCliente extends HttpServlet {
+@WebServlet("/DeleteIngrediente")
+public class DeleteIngrediente extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public UpdateCliente() {
+    public DeleteIngrediente() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,33 +29,20 @@ public class UpdateCliente extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-
+		IngredienteModelo im = new IngredienteModelo();
+		
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		im.delete(id);
+		
+		response.sendRedirect("PanelIngrediente");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		Cliente cliente = new Cliente();
 		
-		// Obtener parámetros del formulario y actualizar la base de datos
-		int id = Integer.parseInt(request.getParameter("id"));
-        String nombre = request.getParameter("nombre");
-        String telefono = request.getParameter("telefono");
-        String email = request.getParameter("email");
-        
-        //introducir los datos en el objeto
-        cliente.setId(id);
-        cliente.setNombre(nombre);
-        cliente.setEmail(email);
-        cliente.setTelefono(telefono);
-        
-        ClienteModelo cm = new ClienteModelo();
-        
-        cm.update(cliente);
-
-        response.sendRedirect("PanelCliente");
 	}
 
 }
