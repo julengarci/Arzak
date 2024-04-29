18fdb990782d3d98acd39a4a54abe199cd467e80
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-04-29 09:44:54 +0200

    commit

---
+++ src/main/java/controlador/DeletePlato.java
@@ -0,0 +1,49 @@
+package controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.IngredienteModelo;
+import modelo.PlatoModelo;
+
+/**
+ * Servlet implementation class DeletePlato
+ */
+@WebServlet("/DeletePlato")
+public class DeletePlato extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeletePlato() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PlatoModelo pm = new PlatoModelo();
+		
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		pm.delete(id);
+		
+		response.sendRedirect("PanelPlato");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
